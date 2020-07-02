c58d743adc19320e6ad4dd9da3da0615a89ed655
Glauber Brack <glauber@brack.com.br>
2020-07-02 03:46:43 -0300

    use of ranges

---
+++ ranges.py
@@ -0,0 +1,14 @@
+"""
+RANGES
+
+ - We MUST need to know 'for' loops to use 'range'
+ - We need to know 'range' to work better with 'for' loops
+"""
+
+# If we not set the initial number, the current value is equal 0 (zero)
+for num in range(10):
+    print(num)
+
+# Range with initial value (5) and final value (100), using an increment (10)
+for n in range(5, 100, 10):
+    print(n)
